fc4188c4eb9da39e56ae69856217222beec3229a
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-06-25 11:19:58 -0300

    Iniciando projeto

---
+++ Projeto Corrigido/index.js
@@ -0,0 +1,10 @@
+let campoUser = $("user");
+let campoSenha = $("pass");
+
+$("form").submit(function (e) {
+    e.preventDefault();
+    if ($(campoUser).val() === "") {
+    }
+    if ($(campoSenha).val() === "") {
+    }
+});
